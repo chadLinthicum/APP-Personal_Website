42c953737ecd6d7935785c9f0447e983f481e951
Chad Linthicum <portableanswers@gmail.com>
2023-01-18 09:04:02 -0500

    Added LF and SD subdomains

---
+++ littleflame.epizy.com/htdocs/ninjapad/config/settings.js
@@ -0,0 +1,31 @@
+// Show/hide debug messages
+const DEBUG = true;
+
+// Virtual analog deadzone
+const DEADZONE = 2; // vmin
+
+// Face buttons angle
+const GAMEBOY_LAYOUT = true;
+
+// Emulation screen
+const EMULATION_SCREEN = "emu-screen";
+const EMULATION_DISPLAY = "emu-canvas";
+
+// Emulation settings
+const SYSTEM = "nes";
+const EMULATOR = "jsnes";
+const DEFAULT_ROM = "roms/LF.nes";
+
+// User experience
+const SINGLE_ROM = true;
+const SAVE_STATES = true;
+const INPUT_RECORDER = true;
+
+// Owner info
+const HOMEPAGE = "https://twitter.com/ninjadynamics";
+const ABOUT = `
+    Follow me on Twitter:<br/>
+    <a href="${HOMEPAGE}" target="_blank">
+    <font color="yellow">ninjadynamics</font>
+    </a>
+`;
