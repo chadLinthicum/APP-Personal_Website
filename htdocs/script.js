918814ec2b740f25d97c9b929d0aeb691b0e34ac
Chad Linthicum <portableanswers@gmail.com>
2023-02-03 18:52:35 -0500

    Hamburger icon converted to gif.

--- htdocs/script.js
+++ htdocs/script.js
@@ -1,6 +1,12 @@
 let hamburger = document.getElementById("hamburger");
 let popout = document.getElementById("popout");
+
 hamburger.addEventListener("click", function () {
   popout.classList.toggle("Popout_Hide");
   popout.classList.toggle("Popout_Show");
+  if (hamburger.src.includes("eggShake.gif")) {
+    hamburger.src = "assets/eggHatch.gif";
+  } else {
+    hamburger.src = "assets/eggShake.gif";
+  }
 });
