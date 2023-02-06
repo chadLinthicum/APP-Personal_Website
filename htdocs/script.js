82e818a5b3f8b600c860bc8abdb67a9213dc611f
Chad Linthicum <portableanswers@gmail.com>
2023-02-06 18:45:51 -0500

    Main content margin-top now adjusted based on Navbar height. Runs on load and window resize.

--- htdocs/script.js
+++ htdocs/script.js
@@ -1,12 +1,38 @@
-let hamburger = document.getElementById("hamburger");
-let nav = document.getElementById("nav");
+$(document).ready(function () {
+  setMainContentMarginTop();
+});
 
-hamburger.addEventListener("click", function () {
-  nav.classList.toggle("Popout_Hide");
-  nav.classList.toggle("Popout_Show");
+window.onresize = setMainContentMarginTop;
+
+let hamburger = document.getElementById("Hamburger");
+let nav = document.getElementById("Nav");
+
+function toggleNav() {
+  nav.classList.toggle("popout_hide");
+  nav.classList.toggle("popout_show");
   if (hamburger.src.includes("eggShake.gif")) {
     hamburger.src = "assets/eggHatch.gif";
   } else {
     hamburger.src = "assets/eggShake.gif";
   }
+}
+
+hamburger.addEventListener("click", function () {
+  toggleNav();
 });
+
+document.querySelectorAll('a[href^="#"').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+    toggleNav();
+  });
+});
+
+function setMainContentMarginTop() {
+  let headerHeight = document.getElementById("Header").offsetHeight;
+  let mainMarginTopPx = document.getElementById("Main");
+  mainMarginTopPx.style.marginTop = headerHeight + "px";
+}
