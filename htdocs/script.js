cc3ec68dae5a57df192f4d2bc0fd80ce151b3ebc
Chad Linthicum <portableanswers@gmail.com>
2023-12-28 18:49:15 -0500

    ITE page updated

--- htdocs/script.js
+++ htdocs/script.js
@@ -46,3 +46,21 @@ $(".nav-link").click(function () {
     scrollTop: $(divId).offset().top - headerHeight,
   });
 });
+
+//Slideshow
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 3500); // Change image every 2 seconds
+}
