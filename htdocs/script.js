de7a4b2177343ba3bfa0d6e68cd9ad98f8c27725
Chad Linthicum <portableanswers@gmail.com>
2023-12-29 16:31:01 -0500

    Clean Sweep page added.

--- htdocs/script.js
+++ htdocs/script.js
@@ -62,5 +62,5 @@ function carousel() {
     slideIndex = 1;
   }
   x[slideIndex - 1].style.display = "block";
-  setTimeout(carousel, 3500); // Change image every 2 seconds
+  setTimeout(carousel, 3500);
 }
